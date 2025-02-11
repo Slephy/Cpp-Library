80efd34d12f9c0be4ef701ffbe8005827bb11204
Slephy <fukuhara.shoma.j6@elms.hokudai.ac.jp>
2025-02-12 00:33:00 +0900

    Add acl-modint

---
+++ lib/modint/acl-modint.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+using namespace std;
+#include <iostream>
+#include <atcoder/modint>
+namespace atcoder
+{
+    template <int m>
+    istream &operator>>(istream &i, static_modint<m, (void *)nullptr> &a)
+    {
+        long long v;
+        i >> v;
+        a = v;
+        return i;
+    }
+    template <int m>
+    istream &operator>>(istream &i, dynamic_modint<m> &a)
+    {
+        long long v;
+        i >> v;
+        a = v;
+        return i;
+    }
+    template <int m>
+    ostream &operator<<(ostream &o, const static_modint<m, (void *)nullptr> &a) { return o << a.val(); }
+    template <int m>
+    ostream &operator<<(ostream &o, const dynamic_modint<m> &a) { return o << a.val(); }
+}
