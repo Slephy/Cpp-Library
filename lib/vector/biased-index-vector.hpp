005a967ca33d38935ef6e1ee56ede357205776d0
Slephy <fukuhara.shoma.j6@elms.hokudai.ac.jp>
2025-02-12 03:23:43 +0900

    Update biased-index-vector

--- lib/vector/biased-index-vector.hpp
+++ lib/vector/biased-index-vector.hpp
@@ -1,23 +1,26 @@
 #include <vector>
 // 添え字に下駄をはかせたvector
 // 'a'や'A'などをゼロとして扱うことを想定している
-template <class T> class Bvector : public vector<T> {
+template <class T, long long BIAS = 0> class Bvector : public vector<T> {
 public:
     using vector<T>::vector;
-    vector<T>::reference operator[](int i) { return vector<T>::operator[](i - this->bias); }
-    vector<T>::const_reference operator[](int i) const { return vector<T>::operator[](i - this->bias); }
-    static long long bias;
-    static void set_bias(long long b) { bias = b; }
+    vector<T>::reference operator[](int i) { return vector<T>::operator[](i - BIAS); }
+    vector<T>::const_reference operator[](int i) const { return vector<T>::operator[](i - BIAS); }
 };
 
-template <class T> inline Bvector<Bvector<T>> Bvector2(size_t i, size_t j, const T &init = T()) {
-    return Bvector<Bvector<T>>(i, Bvector<T>(j, init));
+template <class T, long long BIAS = 0>
+inline Bvector<Bvector<T, BIAS>, BIAS> Bvector2(size_t i, size_t j, const T &init = T()) {
+    return Bvector<Bvector<T, BIAS>, BIAS>(i, Bvector<T, BIAS>(j, init));
 }
-template <class T> inline Bvector<Bvector<Bvector<T>>> Bvector3(size_t i, size_t j, size_t k, const T &init = T()) {
-    return Bvector<Bvector<Bvector<T>>>(i, Bvector<Bvector<T>>(j, Bvector<T>(k, init)));
+template <class T, long long BIAS = 0>
+inline Bvector<Bvector<Bvector<T, BIAS>, BIAS>, BIAS> Bvector3(size_t i, size_t j, size_t k, const T &init = T()) {
+    return Bvector<Bvector<Bvector<T, BIAS>, BIAS>, BIAS>(
+        i, Bvector<Bvector<T, BIAS>, BIAS>(j, Bvector<T, BIAS>(k, init)));
 }
-template <class T>
-inline Bvector<Bvector<Bvector<Bvector<T>>>> Bvector4(size_t i, size_t j, size_t k, size_t l, const T &init = T()) {
-    return Bvector<Bvector<Bvector<Bvector<T>>>>(
-        i, Bvector<Bvector<Bvector<T>>>(j, Bvector<Bvector<T>>(k, Bvector<T>(l, init))));
+template <class T, long long BIAS = 0>
+inline Bvector<Bvector<Bvector<Bvector<T, BIAS>, BIAS>, BIAS>, BIAS> Bvector4(size_t i, size_t j, size_t k, size_t l,
+                                                                              const T &init = T()) {
+    return Bvector<Bvector<Bvector<Bvector<T, BIAS>, BIAS>, BIAS>, BIAS>(
+        i, Bvector<Bvector<Bvector<T, BIAS>, BIAS>, BIAS>(
+               j, Bvector<Bvector<T, BIAS>, BIAS>(k, Bvector<T, BIAS>(l, init))));
 }
