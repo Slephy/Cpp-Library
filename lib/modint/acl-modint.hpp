a7a91da11c0ed69233c386a6117e3dbfc0ac6663
Slephy <fukuhara.shoma.j6@elms.hokudai.ac.jp>
2025-03-18 17:31:17 +0900

    commit

--- lib/modint/acl-modint.hpp
+++ lib/modint/acl-modint.hpp
@@ -1,28 +1,21 @@
 #pragma once
 
 using namespace std;
-#include <iostream>
 #include <atcoder/modint>
-namespace atcoder
-{
-    template <int m>
-    istream &operator>>(istream &i, static_modint<m, (void *)nullptr> &a)
-    {
-        long long v;
-        i >> v;
-        a = v;
-        return i;
-    }
-    template <int m>
-    istream &operator>>(istream &i, dynamic_modint<m> &a)
-    {
-        long long v;
-        i >> v;
-        a = v;
-        return i;
-    }
-    template <int m>
-    ostream &operator<<(ostream &o, const static_modint<m, (void *)nullptr> &a) { return o << a.val(); }
-    template <int m>
-    ostream &operator<<(ostream &o, const dynamic_modint<m> &a) { return o << a.val(); }
+#include <iostream>
+namespace atcoder {
+template <int m> istream &operator>>(istream &i, static_modint<m, (void *)nullptr> &a) {
+    long long v;
+    i >> v;
+    a = v;
+    return i;
 }
+template <int m> istream &operator>>(istream &i, dynamic_modint<m> &a) {
+    long long v;
+    i >> v;
+    a = v;
+    return i;
+}
+template <int m> ostream &operator<<(ostream &o, const static_modint<m, (void *)nullptr> &a) { return o << a.val(); }
+template <int m> ostream &operator<<(ostream &o, const dynamic_modint<m> &a) { return o << a.val(); }
+}  // namespace atcoder
