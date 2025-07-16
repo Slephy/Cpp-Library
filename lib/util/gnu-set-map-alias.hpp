0018688464be2688ab86d95c035e6f16f8b304a6
Slephy <fukuhara.shoma.j6@elms.hokudai.ac.jp>
2025-07-16 19:46:55 +0900

    Rename alias

--- lib/util/gnu-set-map-alias.hpp
+++ lib/util/gnu-set-map-alias.hpp
@@ -4,6 +4,6 @@
 #include "../core/core.hpp"
 using namespace __gnu_pbds;
 template <class T, class Compare = std::less<T>>
-using gnuSet = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+using GNUSet = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
 template <class T, class U, class Compare = std::less<T>>
-using gnuMap = tree<T, U, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+using GNUMap = tree<T, U, Compare, rb_tree_tag, tree_order_statistics_node_update>;
