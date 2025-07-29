3053ceadf1fe61fe7c2766aba786a514be8fd65c
Slephy <fukuhara.shoma.j6@elms.hokudai.ac.jp>
2025-07-29 20:45:01 +0900

    Refactor

--- lib/util/binary-search-integer.hpp
+++ lib/util/binary-search-integer.hpp
@@ -1,13 +1,10 @@
 #pragma once
 #include "../core/core.hpp"
 
-template <class Judgement> long long binary_search_integer(long long ok, long long ng, Judgement judge) {
-    auto need_continue = [&]() -> bool {
-        if(ok > ng) return ((__int128_t)ok - (__int128_t)ng > 1);
-        else return ((__int128_t)ng - (__int128_t)ok > 1);
-    };
-    while(need_continue()) {
-        ll mid = ((__int128_t)ok + (__int128_t)ng) / 2;
+template <class Judgement> ll binary_search_integer(ll ok, ll ng, Judgement judge) {
+    while(true) {
+        ll mid = midpoint(ok, ng);
+        if(mid == ok || mid == ng) break;
         if(judge(mid)) ok = mid;
         else ng = mid;
     }
