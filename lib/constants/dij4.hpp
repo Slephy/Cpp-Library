5544fdbdcbbe2056311dea2b09147264eab0651d
Slephy <fukuhara.shoma.j6@elms.hokudai.ac.jp>
2025-07-31 04:35:57 +0900

    Add

---
+++ lib/constants/dij4.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "../core/core.hpp"
+// ↓→↑←
+const vector<int> di = {1, 0, -1, 0};
+const vector<int> dj = {0, 1, 0, -1};
---
+++ lib/constants/dij8.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "../core/core.hpp"
+// ↓↘→↗↑↖←↙
+const vector<int> di = {1, 1, 0, -1, -1, -1, 0, 1};
+const vector<int> dj = {0, 1, 1, 1, 0, -1, -1, -1};
