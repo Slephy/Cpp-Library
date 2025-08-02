1a2f14b01881de75375fb9125cfc6012e3b1dc0a
Slephy <fukuhara.shoma.j6@elms.hokudai.ac.jp>
2025-08-02 23:42:19 +0900

    Add power

--- lib/math/power.hpp
+++ lib/math/power.hpp
@@ -1,10 +1,31 @@
 #pragma once
+#include "../concepts/modint_concept.hpp"
 #include "../core/core.hpp"
 
-template <typename T>
-concept HasPowMember = requires(T a, ll b) {
-    { a.pow(b) };
-};
+// modint
+template <IsModint T, integral U> T power(T val, U exp) { return val.pow(exp); }
+template <IsModint T, IsModint U> T power(T val, U exp) { return val.pow(exp.val()); }
 
-// 主に modint を想定
-template <HasPowMember T> T power(T val, ll exp) { return val.pow(exp); }
+// integral
+template <class T, integral U>
+    requires(!IsModint<T> && integral<T>)
+T power(T val, U exp) {
+    if(exp < 0) {
+        throw invalid_argument("Negative exponent not supported for integral types.");
+    }
+    T result = 1;
+    while(true) {
+        if(exp % 2 == 1) result *= val;
+        exp /= 2;
+        if(exp > 0) val *= val;
+        else break;
+    }
+    return result;
+}
+
+// floating point
+template <class T, class U>
+    requires(!IsModint<T> && floating_point<T>)
+T power(T val, U exp) {
+    return pow(val, exp);
+}
