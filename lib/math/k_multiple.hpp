bfbd0fefb533e734c0ad0ec0cef3249a93b1c007
Slephy <fukuhara.shoma.j6@elms.hokudai.ac.jp>
2025-08-04 16:08:06 +0900

    change argument's name

--- lib/math/k_multiple.hpp
+++ lib/math/k_multiple.hpp
@@ -4,25 +4,25 @@
 namespace slephy_k_multiple {
 // O(1)
 // where 0 <= inf <= sup
-ll _k_multiple_between(ll inf, ll sup, ll n) {
+ll _k_multiple_between(ll inf, ll sup, ll k) {
     assert(0 <= inf && inf <= sup);
-    return floor_div(sup, n) - floor_div(inf - 1, n);
+    return floor_div(sup, k) - floor_div(inf - 1, k);
 }
 
 // O(1)
 // where inf <= sup
 // return the number of multiple of n betweem [inf, sup];
-ll k_multiple_between(ll inf, ll sup, ll n) {
+ll k_multiple_between(ll inf, ll sup, ll k) {
     if(inf > sup) return 0;
-    if(inf <= 0 && sup <= 0) return _k_multiple_between(-sup, -inf, n);
-    else if(inf <= 0 && sup >= 0) return _k_multiple_between(0, -inf, n) + _k_multiple_between(0, sup, n) - 1;
-    else return _k_multiple_between(inf, sup, n);
+    if(inf <= 0 && sup <= 0) return _k_multiple_between(-sup, -inf, k);
+    else if(inf <= 0 && sup >= 0) return _k_multiple_between(0, -inf, k) + _k_multiple_between(0, sup, k) - 1;
+    else return _k_multiple_between(inf, sup, k);
 }
 
 // O(1)
 // where inf <= sup
-// return the number of nk + r betweem [inf, sup] (where k is integer)
-ll k_multiple_between(ll inf, ll sup, ll n, ll r) { return k_multiple_between(inf - r, sup - r, n); }
+// return the number of nk + r betweem [inf, sup] (where n is integer)
+ll k_multiple_between(ll inf, ll sup, ll k, ll r) { return k_multiple_between(inf - r, sup - r, k); }
 
 };  // namespace slephy_k_multiple
 using slephy_k_multiple::k_multiple_between;
